a5b8415447f2cd1cffb42410f3f6283a57713346
tpatel1781 <tpatel1781@gmail.com>
2019-07-03 14:46:11 -0400

    Started making server with GraphQL. User only has name field. Add list places field.

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,44 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const { GraphQLServer } = require('graphql-yoga')
+const mongoose = require('mongoose'), Schema = mongoose.Schema
+// const UserSchema = require('./User').schema;
+// const PlaceSchema = require('./Place').schema;
 
-app.get('/', (req, res) => res.send('Hello world!'))
-app.listen(port, () => console.log('Example app listening on port ${port}!'))
+// Connect to the 'testPlacesDb' database
+mongoose.connect("mongodb://localhost/testPlacesDb")
+
+// Defining the database schema
+const User = mongoose.model("User", {
+	name: String
+});
+
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+  }
+  type User {
+	id: ID!
+	name: String!
+  }
+  type Mutation {
+	createUser(name: String!): User!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: (_, { name }) => `Hello ${name || 'World'}`,
+  },
+  Mutation: {
+	createUser: async (_, { name }) => {
+		const user = new User({ name });
+		await user.save(); // Adds the new user to the database
+		return user;
+	}
+  }
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers })
+// Start the server once the database connection has been established
+mongoose.connection.once("open", function() {
+	server.start(() => console.log('Server is running on localhost:4000'))
+});
