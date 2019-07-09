1307c5e53dc1bfa4725bbf8ff0daa10e54eb075a
tpatel1781 <tpatel1781@gmail.com>
2019-07-09 14:38:49 -0400

    Changed ID for Users to a username, modified existing endpoints, added getUser endpoint

--- backend/index.js
+++ backend/index.js
@@ -19,49 +19,70 @@ var PlaceSchema = new Schema({
   rating: Number
 });
 var UserSchema = new Schema({
+  _id: String,
   name: String,
-  visitedPlaces: [PlaceSchema]
+  visitedPlaces: [PlaceSchema],
+  groups: [[String]]
 });
 var Place = mongoose.model('Place', PlaceSchema);
 var User = mongoose.model('User', UserSchema);
 
 // API Routes
 app.post('/addUser', async function(req, res) {
-  var user = new User({ name: req.body.name, visitedPlaces: [] });
-  await user.save();
-  res.send(user);
-  console.log('Added user ' + req.body.name);
+  var returnMessage;
+  var user = new User({ _id: req.body.username, name: req.body.name, visitedPlaces: [], groups: [[]] });
+  await user.save(function (err) {
+    console.log("Error: " + err);
+    if (err) {
+      res.send("Must choose a username that has not been taken by another user");
+    } else {
+      res.send(user);
+      console.log('Added user ' + req.body.name);
+    }
+  });
+});
+app.post('/addGroup', async function(req, res) {
+  
+});
+app.get('/getUser', async function(req, res) {
+  await User.findOne({ _id: req.body.username }, async function(err, results) {
+    if (err || !results) {
+      res.send('Cannot find user: \'' + req.body.username + '\'');
+    } else {
+      res.send(results);
+    }
+  });
 });
 app.post('/addPlace', async function(req, res) {
-  await User.findOne({ _id: req.body.userID }, async function(err, results) {
+  await User.findOne({ _id: req.body.username }, async function(err, results) {
     if (err) return err;
     const correctPlace = []
     // Get the list of existing 
     results.visitedPlaces.forEach(place => {
       if (place.googleID === req.body.googleID) {
         correctPlace.push(place);
-        break;
       }
     });
     if (!correctPlace.length) {
       // User does not already have this place, add it to visitedPlaces
       console.log("User does not already have this place");
       var place = new Place({ googleID: req.body.googleID, name: req.body.name, rating: req.body.rating });
-      await User.findOneAndUpdate({ _id: req.body.userID }, { $push: { visitedPlaces: place }});
+      await User.findOneAndUpdate({ _id: req.body.username }, { $push: { visitedPlaces: place }});
+      res.send('Added place (Google ID): ' + req.body.googleID + ' to user ' + req.body.username);
     } else {
       // User already has this place, update existing item
       console.log("User already has this place");
-      await User.findOneAndUpdate({ "visitedPlaces.googleID": req.body.googleID, _id: req.body.userID }, {$set: {
+      await User.findOneAndUpdate({ "visitedPlaces.googleID": req.body.googleID, _id: req.body.username }, {$set: {
         'visitedPlaces.$.name': req.body.name,
         'visitedPlaces.$.rating': req.body.rating
-      }})
+      }});
+      res.send('Updated place (Google ID): ' + req.body.googleID + ' for user ' + req.body.username);
     }
   });
-  res.send('Added place ' + req.body.name + ' to user ' + req.body.userID);
 });
 app.delete('/removePlace', async function(req, res) {
-  await User.update( { _id: req.body.userID }, { $pull: { visitedPlaces: { _id: req.body.placeID }}});
-  res.send('Successfully removed place');
+  await User.update( { _id: req.body.username }, { $pull: { visitedPlaces: { googleID: req.body.googleID }}});
+  res.send('Successfully removed place (Google ID): ' + req.body.googleID);
 });
 
 mongoose.connection.once("open", function() {
