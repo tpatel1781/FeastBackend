5cef90c1664731a35664ab131dd25c383e07d1d7
tpatel1781 <tpatel1781@gmail.com>
2019-08-22 12:22:36 -0400

    Added console logs to votePollPlace

--- backend/app.js
+++ backend/app.js
@@ -229,6 +229,7 @@ app.post('/addPollPlaces', async function (req, res) {
 app.post('/votePollPlace', async function (req, res) {
 	var vote = (req.body.isUpvote) ? 1 : -1;
 	const string = "pollPlaces." + req.body.index + ".votes"
+	console.log("String: " + string)
 	await Group.findOneAndUpdate({ _id: req.body.groupID }, { $inc: { string: vote}});
 	res.send("Changed vote by " + vote);
 });
