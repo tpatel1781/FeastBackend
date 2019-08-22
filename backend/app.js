a64b3b76abbfffcba86730d3688d387af64a6f52
tpatel1781 <tpatel1781@gmail.com>
2019-08-22 13:08:58 -0400

    votePollPlaces

--- backend/app.js
+++ backend/app.js
@@ -228,8 +228,7 @@ app.post('/addPollPlaces', async function (req, res) {
 });
 app.post('/votePollPlace', async function (req, res) {
 	var vote = (req.body.isUpvote) ? 1 : -1;
-	const string = "pollPlaces." + req.body.index + ".votes"
-	console.log("String: " + string)
+	console.log("String: " + req.body.id)
 	await Group.findOneAndUpdate({ _id: req.body.groupID, 'pollPlaces._id' : req.body.id}, { $inc: { 'pollPlaces.$.votes': vote}}, {}, function (error, result) {
 		if (error) {
 			console.log("Vote Poll Place Error: " + error);
